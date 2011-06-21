82e32b35d760092bfeca7aa1ba300893078413c5
kputnam <putnam.kyle@gmail.com>
2011-06-21 03:45:38 -0500

    Enhanced parser to distinguish literals: boolean, string, numeric, etc

--- src/main/scala/com/github/kputnam/fifth/parser/Node.scala
+++ src/main/scala/com/github/kputnam/fifth/parser/Node.scala
@@ -1,12 +1,31 @@
 package com.github.kputnam.fifth.parser
 
-sealed abstract class Node
+import com.github.kputnam.fifth.types.{NumericType, BooleanType, StringType}
 
-case class Word(value: Any) extends Node {
-  override def toString = value.toString
-}
+sealed abstract class Node
 
 case class Quotation(nodes: List[Node]) extends Node {
   override def toString =
-    nodes.mkString("[", ", ", "]")
+    nodes.mkString("[", " ", "]")
+}
+
+case class Word(name: String) extends Node {
+  override def toString = name
+}
+
+sealed abstract class Literal extends Node
+
+case class StringLit(value: String) extends Literal {
+  def toType = StringType
+  override def toString = "\"" + value + "\""
+}
+
+case class BooleanLit(value: Boolean) extends Literal {
+  def toType = BooleanType
+  override def toString = value.toString
+}
+
+case class NumericLit(value: BigDecimal) extends Literal {
+  def toType = NumericType
+  override def toString = value.toString
 }
--- src/main/scala/com/github/kputnam/fifth/parser/Parser.scala
+++ src/main/scala/com/github/kputnam/fifth/parser/Parser.scala
@@ -2,14 +2,24 @@ package com.github.kputnam.fifth.parser
 
 class Parser extends scala.util.parsing.combinator.RegexParsers {
   def topLevel: Parser[Quotation] =
-    rep1(node) ^^ { case xs => Quotation(xs) }
+    rep1(node) ^^ (x => Quotation(x))
 
   def quotation: Parser[Quotation] =
-    "[" ~> rep(node) <~ "]" ^^ { case xs => Quotation(xs) }
+    "[" ~> rep(node) <~ "]" ^^ (x => Quotation(x))
+
+  def string: Parser[StringLit] =
+    "\"" ~> "[^\"]*".r <~ "\"" ^^ (x => StringLit(x))
+
+  def boolean: Parser[BooleanLit] =
+    "true|false".r ^^ { case "true"  => BooleanLit(true)
+                        case "false" => BooleanLit(false) }
+
+  def numeric: Parser[NumericLit] =
+    "[+-]?[0-9]+(?:\\.[0-9]*)?".r ^^ (x => NumericLit(BigDecimal(x)))
 
   def word: Parser[Word] =
-    """[^\s\[\]]+""".r ^^ { case x => Word(x) }
+    "[^\\s\\[\\]]+".r ^^ (x => Word(x))
 
   def node: Parser[Node] =
-    quotation | word
+    quotation | string | boolean | numeric | word
 }
