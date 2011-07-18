a08572c02121633aad584ec48679dfa2d5fda6ac
kputnam <putnam.kyle@gmail.com>
2011-07-18 15:47:26 -0500

    Upgraded to SBT 0.10.0

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "bcat"
+
+version := "0.1"
+
+organization := "com.github.kputnam"
+
+scalaVersion := "2.9.0"
--- project/build/Fifth.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-
-class FifthProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
-    // ...
-}
