a2d308d0eb0b5fbe0c68eb66ab172e62b4df509e
kputnam <putnam.kyle@gmail.com>
2012-01-06 20:39:51 -0600

    Defined fixed-point combinator

--- bee.rb
+++ bee.rb
@@ -117,7 +117,7 @@ module Bee
       @storage = {}
     end
 
-    def define(definition)
+    def add(definition)
       @storage[definition.name] = definition.terms
       self
     end
@@ -154,7 +154,7 @@ module Bee
         when ":"
           nesting.push(Definition.new)
         when ";"
-          dictionary.define(nesting.pop)
+          dictionary.add(nesting.pop)
         else
           nesting.last.append(term(token))
         end
@@ -189,7 +189,7 @@ module Bee
       @dictionary = Dictionary.new
     end
 
-    def run(terms, dictionary)
+    def run(debug, terms, dictionary)
       @dictionary.import(dictionary)
       @input.concat(terms)
 
@@ -197,7 +197,7 @@ module Bee
       trace = []
 
       until (term = @input.shift).nil?
-        trace << [@stack.map(&:inspect).join(" "), term.inspect, @input.map(&:inspect).join(" ")]
+        trace << [@stack.map(&:inspect).join(" "), term.inspect, @input.map(&:inspect).join(" ")] if debug
 
         if term.is_a?(Term) and term.name?
           case term.name
@@ -257,6 +257,11 @@ module Bee
             b = @stack.pop
             a = @stack.pop
             @stack.push(a ? b : c)
+
+          when "="
+            b = @stack.pop
+            a = @stack.pop
+            @stack.push(a == b)
 
           when *%w(+ - * / % ** < <= == >= > >> << & | ^)
             b = @stack.pop
@@ -326,26 +331,23 @@ module Bee
       end
 
       @stack
-    rescue
-      @input.clear
-
-      $stderr.puts $!.to_s.red
-    # $stderr.puts $!.backtrace.map{|s| "\t#{s}" }
     ensure
-      s = trace.map{|_| _[0].length }.max # stack
-      t = trace.map{|_| _[1].length }.max # eval term
-      c = trace.map{|_| _[2].length }.max # continuation
+      unless trace.empty?
+        s = trace.map{|_| _[0].length }.max # stack
+        t = trace.map{|_| _[1].length }.max # eval term
+        c = trace.map{|_| _[2].length }.max # continuation
 
-      maxs, maxt, maxc = trace.inject([0,0,0]) do |(s,t,c), _|
-        [ s > _[0].length ? s : _[0].length,
-          t > _[1].length ? t : _[1].length,
-          c > _[2].length ? c : _[2].length ]
-      end
+        maxs, maxt, maxc = trace.inject([0,0,0]) do |(s,t,c), _|
+          [ s > _[0].length ? s : _[0].length,
+            t > _[1].length ? t : _[1].length,
+            c > _[2].length ? c : _[2].length ]
+        end
 
-      trace.each do |t|
-        $stdout.puts ".. " << t[0].rjust(maxs).yellow <<
-                     " : " << t[1].rjust(maxt).cyan   <<
-                     " : " << t[2]
+        trace.each do |t|
+          $stdout.puts ".. " << t[0].rjust(maxs).yellow <<
+                       " : " << t[1].rjust(maxt).cyan   <<
+                       " : " << t[2]
+        end
       end
     end
 
@@ -357,46 +359,65 @@ end
 $vm = Bee::Interpreter.new
 $p  = Bee::Parser.new
 
-$vm.dictionary.define(Bee::Definition.new("bottles",
+$vm.dictionary.add(Bee::Definition.new("bottles",
   $p.parse('dup dup to_s " bottles" swap cons print 0 == [pop] [1 - bottles] if apply').first))
 
-$vm.dictionary.define(Bee::Definition.new("twice",
+$vm.dictionary.add(Bee::Definition.new("twice",
   $p.parse("dup compose apply").first))
 
-$vm.dictionary.define(Bee::Definition.new("fold",
+$vm.dictionary.add(Bee::Definition.new("fold",
   $p.parse("dup quote compose [fold] compose [quote dup] dip compose unlist").first))
 
-$vm.dictionary.define(Bee::Definition.new("reverse-map",
+$vm.dictionary.add(Bee::Definition.new("reverse-map",
   $p.parse("null [swap] dig compose [cons] compose fold").first))
 
-$vm.dictionary.define(Bee::Definition.new("reverse",
+$vm.dictionary.add(Bee::Definition.new("reverse",
   $p.parse("null [swap cons] fold").first))
 
-$vm.dictionary.define(Bee::Definition.new("map",
+$vm.dictionary.add(Bee::Definition.new("map",
   $p.parse("reverse-map reverse").first))
 
 # Naive version (non tail call)
-#$vm.dictionary.define(Bee::Definition.new("map",
+#$vm.dictionary.add(Bee::Definition.new("map",
 #  $p.parse("swap [pop null] [dig dup dip [swap] dip map swap cons] unlist").first))
 
-$vm.dictionary.define(Bee::Definition.new("length",
+$vm.dictionary.add(Bee::Definition.new("length",
   $p.parse("0 [[pop] dip 1 +] fold").first))
 
 # Naive version (non tail call)
-#$vm.dictionary.define(Bee::Definition.new("length",
+#$vm.dictionary.add(Bee::Definition.new("length",
 #  $p.parse("[0] [pop length 1 +] unlist").first))
 
-$vm.dictionary.define(Bee::Definition.new("sum",
+$vm.dictionary.add(Bee::Definition.new("sum",
   $p.parse("0 [+] fold").first))
 
-#$vm.dictionary.define(Bee::Definition.new("sum",
+#$vm.dictionary.add(Bee::Definition.new("sum",
 #  $p.parse("[0] [swap sum +] unlist").first))
 
-$vm.dictionary.define(Bee::Definition.new("product",
+$vm.dictionary.add(Bee::Definition.new("product",
   $p.parse("1 [*] fold").first))
 
-def bee(unparsed)
-  $vm.run(*$p.parse(unparsed))
+$vm.dictionary.add(Bee::Definition.new("null?",
+  $p.parse("[true] [pop pop false] unlist").first))
+
+$vm.dictionary.add(Bee::Definition.new("cons?",
+  $p.parse("[false] [pop pop true] unlist").first))
+
+# This is like fix f = f (fix f)... references itself
+$vm.dictionary.add(Bee::Definition.new("y'",
+  $p.parse("dup quote [y'] compose quote swap compose apply").first))
+
+$vm.dictionary.add(Bee::Definition.new("!'",
+  $p.parse("swap dup 1 = [pop pop 1] [dup [-1 + swap apply] dip *] if apply").first))
+
+$vm.dictionary.add(Bee::Definition.new("!",
+  $p.parse("[!'] y'").first))
+
+def bee(unparsed, debug = false)
+  $vm.run(debug, *$p.parse(unparsed))
+rescue
+  $vm.input.clear
+  $stderr.puts $!.to_s.red
 end
 
 ################################################################################
