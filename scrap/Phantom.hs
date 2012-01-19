4cef703a3c59885d571e80f50cb942abd67de939
kputnam <putnam.kyle@gmail.com>
2012-01-19 17:37:14 -0600

    Exploring phantom types

---
+++ scrap/Phantom.hs
@@ -0,0 +1,69 @@
+
+-- private
+data PhantomExpr a
+  = PhantomExpr Expr
+  deriving (Eq, Show)
+
+-- private
+data Expr
+  = BinaryExpr    Op Expr Expr
+  | UnaryExpr     Op  Expr
+  | ConstantExpr  String
+  deriving (Eq, Show)
+
+-- private
+data Op 
+  = OpEq
+  | OpGt
+  | OpLt
+  | OpGte
+  | OpLte
+  | OpNe
+  | OpAdd
+  | OpDiv
+  | OpMod
+  | OpMul
+  | OpNeg
+  | OpSub
+  | OpAnd
+  | OpNot
+  | OpOr
+  | OpXor
+  deriving (Eq, Show)
+
+constant :: Show a => a -> PhantomExpr a
+(.+.)    :: Num a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(.-.)    :: Num a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(.*.)    :: Num a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(./.)    :: Num a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(.%.)    :: Num a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(.&.)    :: PhantomExpr Bool -> PhantomExpr Bool -> PhantomExpr Bool
+(.|.)    :: PhantomExpr Bool -> PhantomExpr Bool -> PhantomExpr Bool
+(.^.)    :: PhantomExpr Bool -> PhantomExpr Bool -> PhantomExpr Bool
+(.~.)    :: PhantomExpr Bool -> PhantomExpr Bool
+(.<.)    :: Ord a => PhantomExpr a -> PhantomExpr a -> PhantomExpr Bool
+(.>.)    :: Ord a => PhantomExpr a -> PhantomExpr a -> PhantomExpr Bool
+(.<=.)   :: Ord a => PhantomExpr a -> PhantomExpr a -> PhantomExpr Bool
+(.>=.)   :: Ord a => PhantomExpr a -> PhantomExpr a -> PhantomExpr Bool
+(.==.)   :: Eq a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+(./=.)   :: Eq a => PhantomExpr a -> PhantomExpr a -> PhantomExpr a
+
+constant a = PhantomExpr $ ConstantExpr (show a)
+(.+.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpAdd a b
+(.-.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpSub a b
+(.*.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpMul a b
+(./.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpDiv a b
+(.%.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpMod a b
+(.&.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpAnd a b
+(.|.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpOr  a b
+(.^.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpXor a b
+(.~.)  (PhantomExpr a)                 = PhantomExpr $ UnaryExpr  OpNot a
+(.<.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpLt  a b
+(.>.)  (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpGt  a b
+(.<=.) (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpLte a b
+(.>=.) (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpGte a b
+(.==.) (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpEq  a b
+(./=.) (PhantomExpr a) (PhantomExpr b) = PhantomExpr $ BinaryExpr OpNe  a b
+
+-- Fails to typecheck
+--   constant 1 .==. constant 'x'
