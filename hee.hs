d68f2a9534a854069840b39e9162148dccd5f0aa
kputnam <putnam.kyle@gmail.com>
2012-01-18 18:12:04 -0600

    Work on type system

--- hee.hs
+++ hee.hs
@@ -1,3 +1,9 @@
+-- Adapted from Mark P. Jones' "Typing Haskell in Haskell"
+--   http://web.cecs.pdx.edu/~mpj/thih/thih.pdf
+
+import List (nub, (\\), intersect, union, partition)
+import Monad (msum)
+
 type Id = String
 
 -- Terms are either a composition of one function with another term, or
@@ -58,10 +64,89 @@ tReal = TyConstant "real" KiType
 tChar = TyConstant "char" KiType
 tUnit = TyConstant "()"   KiType
 tPair = TyConstant "(,)"  (KiCons KiType (KiCons KiType KiType))
-tList = TyConstant "[]"   (KiCons KiType (KiCons KiType KiType))
-tWord = TyConstant "(->)" (KiCons KiStack KiStack)
+tList = TyConstant "[]"   (KiCons KiType KiType)
+tFunc = TyConstant "(->)" (KiCons KiStack KiStack)
 
-mkWord :: Stack -> Stack -> Type
-mkWord inp out = TyApplication (TyApplication tWord (TyStack inp)) (TyStack out)
+mkFunc :: Stack -> Stack -> Type
+mkFunc inp out = TyApplication (TyApplication tFunc (TyStack inp)) (TyStack out)
 
+mkList :: Type -> Type
+mkList t = TyApplication tList t
 
+mkPair :: Type -> Type -> Type
+mkPair fst snd = TyApplication (TyApplication tPair fst) snd
+
+class HasKind t where
+  kind :: t -> Kind
+
+instance HasKind Type where
+  kind (TyVariable _ k)     = k
+  kind (TyConstant _ k)     = k
+  kind (TyStack _)          = KiStack
+  kind (TyApplication i _)  = case (kind i) of
+                                (KiCons _ k) -> k
+instance HasKind Kind where
+  kind k = k
+
+instance HasKind Stack where
+  kind t = KiStack
+
+-- TyVariable -> Type
+type Variable     = (Id, Kind)
+type Substitution = [(Variable, Type)]
+
+-- Empty subtitution
+nullSubstitution :: Substitution
+nullSubstitution = []
+
+-- Singleton substitution
+--   kind preserving iff (kind v) == (kind t)
+(+->)    :: Variable -> Type -> Substitution
+(+->) v t = [(v, t)]
+
+class CanSubstitute t where
+  substitute :: Substitution -> t -> t
+  freeVars   :: t -> [Variable]
+
+instance CanSubstitute Type where
+  substitute s (TyApplication i o) = TyApplication (substitute s i) (substitute s o)
+  substitute s (TyStack t)         = TyStack (substitute s t)
+  substitute s (TyVariable id k)   = case lookup (id,k) s of
+                                       Just t  -> t
+                                       Nothing -> TyVariable id k
+  substitute s t = t
+  
+  freeVars (TyApplication i o) = freeVars i `union` freeVars o
+  freeVars (TyStack t)         = freeVars t
+  freeVars (TyVariable id k)   = [(id,k)]
+  freeVars _                   = []
+
+instance CanSubstitute Stack where
+  substitute s (StBottom id) = case lookup (id,KiStack) s of
+                                 Just (TyStack t) -> t
+                                 Just t  -> StBottom id
+                                 Nothing -> StBottom id
+  substitute s t = t
+  
+  freeVars (StBottom id) = [(id,KiStack)]
+  freeVars _             = []
+
+instance CanSubstitute a => CanSubstitute [a] where
+  substitute s = map (substitute s)
+  freeVars     = nub . concat . map freeVars
+
+-- Composition of substitutions
+--   substitute (a @@ b) = substitute a . substitute b
+infixr   4 @@
+(@@)    :: Substitution -> Substitution -> Substitution
+(@@) a b = [(v, substitute a t) | (v, t) <- b] ++ b
+
+-- Composition of substitutions
+merge    :: Monad m => Substitution -> Substitution -> m Substitution
+merge a b = if match
+            then return (a ++ b)
+            else fail "merge failed"
+  where match = all (\v -> substitute a (tvar v) == substitute b (tvar v))
+                    (map fst a `intersect` map fst b)
+        tvar (id,k) = TyVariable id k
+        svar (id,k) = StBottom id
