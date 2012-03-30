04134476bfe48a2ff21333cd5d6cbd1d2ddb87fe
kputnam <putnam.kyle@gmail.com>
2012-03-30 00:21:52 -0500

    Replace with Attoparsec

--- src/main/haskell/Hee/Parser.hs
+++ src/main/haskell/Hee/Parser.hs
@@ -1,64 +1,96 @@
-import Text.ParserCombinators.Parsec
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hee.Parser
+  where
+
+import Prelude hiding (takeWhile)
+import Control.Applicative ((<|>), (*>), (<$>))
+
+import Data.Attoparsec
+import Data.ByteString (cons)
+import Data.Word
 import Hee.Terms
 
-heeExpr :: Parser Term
-heeExpr = heeQuote
-      <|> heeCompose
-      <|> heeEmpty
+heeExpr = undefined
+--    <|> heeQuote
+--    <|> heeName
+--    <|> heeString
+--    <|> heeNumber
+--    <|> heeChar
 
-heeEmpty :: Parser Term
-heeEmpty = eof >> return TmEmpty
+heeQuote = do word8 91
+              word8 93
+              return ()
 
-heeQuote :: Parser Term
-heeQuote = do char '['
-              q <- heeExpr
-              char ']'
-              return $ TmQuote q
+heeName = do head <- satisfy $ notInClass "[] \t\r\n'\""
+             tail <- takeWhile $ notInClass "[] \t\r\n"
+             return $ cons head tail
 
-heeName :: Parser Term
-heeName = do head <- noneOf "0123456789['\" \t\n"
-             tail <- many (noneOf " \t\n]")
-             return $ TmName (head:tail)
+heeNumber = undefined --
 
-heeLiteral :: Parser Literal
-heeLiteral = heeString
-         <|> heeChar
-         <|> try (string "0x" >> heeInt "0x")
-         <|> try (string "0o" >> heeInt "0o")
-         <|> try (string "0b" >> heeInt "0b")
-         <|> try heeFloat
-         <|> heeInt ""
+heeString = do word8 34
+               word8 34
+               return ()
 
-heeString :: Parser Literal
-heeString = do char '"'
-               cs <- many (noneOf "\"")
-               char '"'
-               return $ LiString cs
+heeChar = do string "'"
+             escape <|> single
+  where single = anyWord8
+        escape = word8 92 >> anyWord8
+        tx  92 = 92 -- '\\
+        tx 110 = 10 -- '\n
+        tx 114 = 13 -- '\r
+        tx 116 = 9  -- '\t
+        tx   c = c
 
-heeChar :: Parser Literal
-heeChar = do char '\''
-             c <- anyChar
-             return $ LiChar c
-
-heeInt :: String -> Parser Literal
-heeInt radix = do digits <- many1 (oneOf "0123456789abcdef")
-                  let parsed = read (radix ++ digits)
-                  return $ LiInt parsed
-
-heeFloat :: Parser Literal
-heeFloat = do whole    <- many1 (oneOf "0123456789")
-              point    <- char '.'
-              fraction <- many (oneOf "0123456789")
-              let parsed = read (whole ++ "." ++ fraction ++ "0")
-              return $ LiFloat parsed
-
-heeTerm :: Parser Term
-heeTerm = heeName
--- <|> literal
-   <|> heeQuote
-
-heeCompose :: Parser Term
-heeCompose = undefined
-
-ws :: Parser ()
-ws = skipMany space
+-- Data.Attoparsec.ByteString
+--
+--   Data.Attoparsec.Internal.Types
+--   T.Fail
+--   T.Done
+--   T.Partial
+--
+--   Data.Attoparsec.Combinator
+--   choice
+--   count
+--   option
+--   many1
+--   manyTill
+--   sepBy
+--   sepBy1
+--   skipMany
+--   skipMany1
+--   eitherP
+--
+--   Data.Attoparsec.ByteString.Internal
+--   I.try
+--   I.<?>
+--   I.parseOnly
+--   I.word8
+--   I.anyWord8
+--   I.notWord8
+--   I.inClass
+--   I.notInClass
+--   I.storable
+--   I.satisfy
+--   I.satisfyWith
+--   I.skip
+--   I.skipWhile
+--   I.take
+--   I.scan
+--   I.string
+--   I.stringTransform
+--   I.takeWhile
+--   I.takeWhile1
+--   I.takeTill
+--   I.takeByteString
+--   I.takeLazyByteString
+--   I.endOfLine
+--   I.endOfInput
+--   I.atEnd
+--
+--   parse
+--   feed
+--   parseWith
+--   parseTest
+--   maybeResult
+--   eitherResult
