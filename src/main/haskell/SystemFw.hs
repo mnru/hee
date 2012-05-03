c8e5316ca8c91c59436236c6cebcf47e3187d6ac
kputnam <putnam.kyle@gmail.com>
2012-05-03 18:52:45 -0500

    Implement kind system

--- src/main/haskell/SystemFw.hs
+++ src/main/haskell/SystemFw.hs
@@ -35,7 +35,7 @@ data Term
   deriving (Eq)
 
 data Type
-  = TVariable Id                  -- α,β         type variable
+  = TVariable Id Kind             -- α,β         type variable
   | TApplication Type Type        -- τ υ         type application
   | TAbstraction Id Kind Type     -- λα:κ. τ     type abstraction
   | TQuantification Id Kind Type  -- ∀α:κ. τ     type quantification
@@ -46,6 +46,21 @@ data Kind
   = KType                         -- ★           kind of manifest type
   | KOperator Kind Kind           -- κ → ι       kind of operator on types
   deriving (Eq)
+
+---------------------------------------------------------------------------
+
+class HasKind a where
+  kind :: a -> Kind
+
+instance HasKind Type where
+  kind (TVariable _ k)         = k
+  kind (TOperator t u)         = KType
+  kind (TQuantification _ _ t) = kind t
+  kind (TAbstraction _ k t)    = KOperator k (kind t)
+  kind (TApplication t _)      = let (KOperator k l) = kind t in l
+
+instance HasKind Kind where
+  kind = id
 
 ---------------------------------------------------------------------------
 
@@ -68,13 +83,13 @@ showTerm n (TmApplication e f)    = "(" ++ showTerm (n+1) e ++ " " ++ showTerm (
 showTerm n (TmUAbstraction a k e) = "(Λ" ++ showTId a  ++ ":" ++ showKind (n+1) k ++ ". " ++ showTerm 0 e ++ ")"
 showTerm n (TmAbstraction x t e)  = "(λ" ++ showTmId x ++ ":" ++ showType (n+1) t ++ ". " ++ showTerm 0 e ++ ")"
 
-showType 0 (TVariable a)            = showTId a
+showType 0 (TVariable a _)          = showTId a
 showType 0 (TOperator t u)          = showType 1 t ++ " → " ++ showType 1 u
 showType 0 (TApplication a b)       = showType 1 a ++ " "   ++ showType 1 b
 showType 0 (TAbstraction a k t)     = "λ" ++ showTId a ++ ":" ++ showKind 0 k ++ ". " ++ showType 0 t
 showType 0 (TQuantification a k t)  = "∀" ++ showTId a ++ ":" ++ showKind 0 k ++ ". " ++ showType 0 t
 
-showType n (TVariable a)            = showTId a
+showType n (TVariable a _)          = showTId a
 showType n (TOperator t u)          = "(" ++ showType (n+1) t ++ " → " ++ showType (n+1) u ++ ")"
 showType n (TApplication a b)       = "(" ++ showType (n+1) a ++ " "   ++ showType (n+1) b ++ ")"
 showType n (TAbstraction a k t)     = "(λ" ++ showTId a ++ ":" ++ showKind (n+1) k ++ ". " ++ showType 0 t ++ ")"
@@ -100,43 +115,43 @@ instance Show Kind where
 -- Bool : ★
 -- Bool = ∀α. α → α → α
 bool  = TQuantification 0 KType $
-          TOperator (TVariable 0) $
-            TOperator (TVariable 0) (TVariable 0)
+          TOperator (TVariable 0 KType) $
+            TOperator (TVariable 0 KType) (TVariable 0 KType)
 
 --tru : ∀α. α → α → α
 --tru = Λα:*. λt:α. λf:α. t
 tru = TmUAbstraction 0 KType $
-         TmAbstraction 19 (TVariable 0) $
-           TmAbstraction 5 (TVariable 0) $
+         TmAbstraction 19 (TVariable 0 KType) $
+           TmAbstraction 5 (TVariable 0 KType) $
              TmVariable 19
 
 --fls : ∀α. α → α → α
 --fls = Λα:*. λt:α. λf:α. f
 fls = TmUAbstraction 0 KType $
-        TmAbstraction 19 (TVariable 0) $
-          TmAbstraction 5 (TVariable 0) $
+        TmAbstraction 19 (TVariable 0 KType) $
+          TmAbstraction 5 (TVariable 0 KType) $
             TmVariable 5
 
 -- Nat : ★
 -- Nat = ∀α. α → (α → α) → α
 nat = TQuantification 0 KType $
-        TOperator (TVariable 0) $
+        TOperator (TVariable 0 KType) $
           TOperator
-            (TOperator (TVariable 0) (TVariable 0))
-            (TVariable 0)
+            (TOperator (TVariable 0 KType) (TVariable 0 KType))
+            (TVariable 0 KType)
 
 -- zero : ∀α. α → (α → α) → α
 -- zero = Λα:*. λa:α. λf:α → α. a
 zero =  TmUAbstraction 0 KType $
-          TmAbstraction 0 (TVariable 0) $
-            TmAbstraction 5 (TOperator (TVariable 0) (TVariable 0)) $
+          TmAbstraction 0 (TVariable 0 KType) $
+            TmAbstraction 5 (TOperator (TVariable 0 KType) (TVariable 0 KType)) $
               TmVariable 0
 
 -- succ : ∀α. α → (α → α) → α
 -- succ = Λα:*. λa:α. λf:α → α. f a
 succ =  TmUAbstraction 0 KType $
-          TmAbstraction 0 (TVariable 0) $
-            TmAbstraction 5 (TOperator (TVariable 0) (TVariable 0)) $
+          TmAbstraction 0 (TVariable 0 KType) $
+            TmAbstraction 5 (TOperator (TVariable 0 KType) (TVariable 0 KType)) $
               TmApplication (TmVariable 5) (TmVariable 0)
 
 -- Product : ★ → (★ → ★)
@@ -145,9 +160,9 @@ pair =  TAbstraction 0 KType $
           TAbstraction 1 KType $
             TQuantification 15 KType $
               TOperator
-                (TOperator (TVariable 0) $
-                  TOperator (TVariable 1) (TVariable 15))
-                (TVariable 15)
+                (TOperator (TVariable 0 KType) $
+                  TOperator (TVariable 1 KType) (TVariable 15 KType))
+                (TVariable 15 KType)
 
 -- fst : ∀α:*. ∀β:*. ∀R:*. (α → β → R) → α
 -- fst = Λα:*. Λβ:*. λp:(∀R:*. α → β → R). p α (λa:α. λb:β. a)
@@ -155,12 +170,12 @@ fst = TmUAbstraction 0 KType $
         TmUAbstraction 1 KType $
           TmAbstraction 15
             (TQuantification 15 KType
-              (TOperator (TVariable 0)
-                (TOperator (TVariable 1) (TVariable 15))))
+              (TOperator (TVariable 0 KType)
+                (TOperator (TVariable 1 KType) (TVariable 15 KType))))
             (TmApplication
-              (TmUApplication (TmVariable 15) (TVariable 0))
-              (TmAbstraction 0 (TVariable 0)
-                (TmAbstraction 1 (TVariable 1)
+              (TmUApplication (TmVariable 15) (TVariable 0 KType))
+              (TmAbstraction 0 (TVariable 0 KType)
+                (TmAbstraction 1 (TVariable 1 KType)
                   (TmVariable 0))))
 
 -- snd : ∀α:*. ∀β:*. ∀R:*. (α → β → R) → β 
@@ -169,12 +184,12 @@ snd = TmUAbstraction 0 KType $
         TmUAbstraction 1 KType $
           TmAbstraction 15
             (TQuantification 15 KType
-              (TOperator (TVariable 0)
-                (TOperator (TVariable 1) (TVariable 15))))
+              (TOperator (TVariable 0 KType)
+                (TOperator (TVariable 1 KType) (TVariable 15 KType))))
             (TmApplication
-              (TmUApplication (TmVariable 15) (TVariable 0))
-              (TmAbstraction 0 (TVariable 0)
-                (TmAbstraction 1 (TVariable 1)
+              (TmUApplication (TmVariable 15) (TVariable 0 KType))
+              (TmAbstraction 0 (TVariable 0 KType)
+                (TmAbstraction 1 (TVariable 1 KType)
                   (TmVariable 1))))
 
 -- Sum : ★ → (★ → ★)
@@ -182,18 +197,18 @@ snd = TmUAbstraction 0 KType $
 sum = TAbstraction 0 KType $
         TAbstraction 1 KType $
           TQuantification 2 KType $
-            TOperator (TOperator (TVariable 0) (TVariable 2)) $
-              TOperator (TOperator (TVariable 1) (TVariable 2)) $
-                TVariable 2
+            TOperator (TOperator (TVariable 0 KType) (TVariable 2 KType)) $
+              TOperator (TOperator (TVariable 1 KType) (TVariable 2 KType)) $
+                TVariable 2 KType
 
 -- inL : ∀α:*. ∀β:*. α → (∀R:*. (α → R) → (β → R) → R) → α
 -- inL = Λα:*. Λβ:*. ∀R:*. λa:α. λleft:(α → R). λright:(β → R). left a
 inl = TmUAbstraction 0 KType $
         TmUAbstraction 1 KType $
           TmUAbstraction 2 KType $
-            TmAbstraction 0 (TVariable 0) $
-              TmAbstraction 1 (TOperator (TVariable 0) (TVariable 2)) $
-                TmAbstraction 2 (TOperator (TVariable 1) (TVariable 2)) $
+            TmAbstraction 0 (TVariable 0 KType) $
+              TmAbstraction 1 (TOperator (TVariable 0 KType) (TVariable 2 KType)) $
+                TmAbstraction 2 (TOperator (TVariable 1 KType) (TVariable 2 KType)) $
                   TmApplication (TmVariable 1) (TmVariable 0)
 
 -- inL : ∀α:*. ∀β:*. α → (∀R:*. (α → R) → (β → R) → R) → β
@@ -201,9 +216,9 @@ inl = TmUAbstraction 0 KType $
 inr = TmUAbstraction 0 KType $
         TmUAbstraction 1 KType $
           TmUAbstraction 2 KType $
-            TmAbstraction 0 (TVariable 1) $
-              TmAbstraction 1 (TOperator (TVariable 0) (TVariable 2)) $
-                TmAbstraction 2 (TOperator (TVariable 1) (TVariable 2)) $
+            TmAbstraction 0 (TVariable 1 KType) $
+              TmAbstraction 1 (TOperator (TVariable 0 KType) (TVariable 2 KType)) $
+                TmAbstraction 2 (TOperator (TVariable 1 KType) (TVariable 2 KType)) $
                   TmApplication (TmVariable 2) (TmVariable 0)
 
 -- List : ★ → ★
@@ -212,33 +227,33 @@ list =  TAbstraction 0 KType $
           TQuantification 1 KType $
             TOperator
               (TOperator
-                (TVariable 0)
-                (TOperator (TVariable 1) (TVariable 1)))
-              (TOperator (TVariable 1) (TVariable 1))
+                (TVariable 0 KType)
+                (TOperator (TVariable 1 KType) (TVariable 1 KType)))
+              (TOperator (TVariable 1 KType) (TVariable 1 KType))
 
 -- null : ∀α:*. ∀R:*. (α → R → R) → R → R
 -- null = Λα:*. ∀R:*. λf:(α → R → R). λr:R. r
 null =  TmUAbstraction 0 KType $
           TmUAbstraction 1 KType $
             TmAbstraction 0
-              (TOperator (TVariable 0) $
-                TOperator (TVariable 1) (TVariable 1))
-              (TmAbstraction 1 (TVariable 1) $
+              (TOperator (TVariable 0 KType) $
+                TOperator (TVariable 1 KType) (TVariable 1 KType))
+              (TmAbstraction 1 (TVariable 1 KType) $
                 TmVariable 1)
 
 -- cons : ∀α:*. ∀R:*. α → ((α → R → R) → R → R) → ((α → R → R) → R → R)
 -- cons = Λα:*. ∀R:*. λhead:α. λtail:(α → R → R) → R → R. λf:(α → R → R). λr:R. f head (tail f r)
 cons =  TmUAbstraction 0 KType $
           TmUAbstraction 1 KType $
-            TmAbstraction 0 (TVariable 0) $
+            TmAbstraction 0 (TVariable 0 KType) $
               TmAbstraction 1
                 (TOperator
-                  (TOperator (TVariable 0) $
-                    TOperator (TVariable 1) (TVariable 1))
-                  (TOperator (TVariable 1) (TVariable 1)))
+                  (TOperator (TVariable 0 KType) $
+                    TOperator (TVariable 1 KType) (TVariable 1 KType))
+                  (TOperator (TVariable 1 KType) (TVariable 1 KType)))
                 (TmAbstraction 2
-                  (TOperator (TVariable 0) $
-                    TOperator (TVariable 1) (TVariable 1))
-                  (TmAbstraction 3 (TVariable 1) $
+                  (TOperator (TVariable 0 KType) $
+                    TOperator (TVariable 1 KType) (TVariable 1 KType))
+                  (TmAbstraction 3 (TVariable 1 KType) $
                     TmApplication (TmApplication (TmVariable 2) (TmVariable 0)) $
                       TmApplication (TmApplication (TmVariable 1) (TmVariable 2)) (TmVariable 3)))
